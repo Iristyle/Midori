3cb1a3f69307ed07b05de76fe07ae4780614f0e6
Iristyle <Iristyle@github>
2012-11-06 10:19:46 -0500

    Use Midoris Nuget.psm1 to pack itself

--- NuGetPack.ps1
+++ NuGetPack.ps1
@@ -19,59 +19,19 @@ function Get-CurrentDirectory
   [IO.Path]::GetDirectoryName((Get-Content function:$thisName).File)
 }
 
-function Get-NugetPath
-{
-  Write-Host 'Executing Get-NugetPath'
-  Get-ChildItem -Path (Get-CurrentDirectory) -Include 'nuget.exe' -Recurse |
-    Select -ExpandProperty FullName -First 1
+Import-Module (Join-Path (Get-CurrentDirectory) 'tools\Nuget.psm1')
+
+$pubParams = @{
+  Include = 'Midori';
+  Path = (Get-CurrentDirectory);
+  Source = $source;
+  ApiKey = $apiKey;
 }
 
-function Restore-Nuget
+if ($operation -eq 'Pack')
 {
-  Write-Host 'Executing Restore-Nuget'
-  $nuget = Get-NugetPath
-
-  if ($nuget -ne $null)
-  {
-      &"$nuget" update -Self | Write-Host
-      return $nuget
-  }
-
-  $nugetPath = Join-Path (Get-CurrentDirectory) 'nuget.exe'
-  (New-Object Net.WebClient).DownloadFile('http://nuget.org/NuGet.exe', $nugetPath)
-
-  return Get-NugetPath
+  $pubParams.KeepPackages = $true;
+  $pubParams.Source = (Get-CurrentDirectory)
 }
 
-function Invoke-Pack
-{
-  $currentDirectory = Get-CurrentDirectory
-  Write-Host "Running against $currentDirectory"
-
-  Get-ChildItem -Path $currentDirectory -Filter *.nuspec -Recurse |
-    ? { $_.FullName -inotmatch 'packages' } |
-    % {
-      $csproj = Join-Path $_.DirectoryName ($_.BaseName + '.csproj')
-      $cmdLine = if (Test-Path $csproj)
-      {
-        &$script:nuget pack "$csproj" -Prop Configuration=Release -Exclude '**\*.CodeAnalysisLog.xml'
-      }
-      else { &$script:nuget pack $_}
-    }
-}
-
-function Invoke-Push
-{
- Get-ChildItem *.nupkg |
-   % {
-     Write-Host "Value of source -> $source"
-     if ($source -eq '') { &$script:nuget push $_ $apiKey }
-     else { &$script:nuget push $_ $apiKey -s $source }
-   }
-}
-
-$script:nuget = Restore-Nuget
-del *.nupkg
-Invoke-Pack
-if ($operation -eq 'Push') { Invoke-Push }
-del *.nupkg
+Publish-NuGetPackage @pubParams
