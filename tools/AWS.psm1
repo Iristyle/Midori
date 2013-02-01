3e36210808b4069fb7f4d57fb60c6c40b689152e
Iristyle <Iristyle@github>
2013-01-31 20:55:31 -0500

    Notes on making AWS stuff work, new features to implement

--- tools/AWS.psm1
+++ tools/AWS.psm1
@@ -1,8 +1,55 @@
+# TODO: add the ability to additionally grab addresses given a tag -- this can
+# be used during continuous builds to identify the server on which to run tests
+
+
+# well, I changed a couple things
+# Don McNamara
+# Jan-24 3:08 PM
+# not sure if they are all needed
+# Don McNamara
+# Jan-24 3:09 PM
+# "&Expires=$expires&Filter.1.Name=tag%3AName&Filter.1.Value.1=$Name&" +
+# Don McNamara
+# Jan-24 3:09 PM
+# forgot to escape the : in the tag:Name part
+# Don McNamara
+# Jan-24 3:09 PM
+# $urlEncoded = (UrlEncode $base64Encoded)
+# Don McNamara
+# Jan-24 3:09 PM
+# removed the .ToUpper() call
+# Ethan Brown
+# Jan-24 3:09 PM
+# yeah... i only used ToUpper because all the examples had upper case hex chars
+# Ethan Brown
+# Jan-24 3:09 PM
+# that was me grasping at straws ;0
+# Don McNamara
+# Jan-24 3:09 PM
+# not sure if it matters or not, but I also changed the version
+# Don McNamara
+# Jan-24 3:09 PM
+# "SignatureMethod=HmacSHA256&SignatureVersion=2&Version=2012-12-01")
+# Ethan Brown
+# Jan-24 3:10 PM
+# and I think that was *before* I removed the HttpUtility.UrlEncode and swapped in the AWS style one
+# Don McNamara
+# Jan-24 3:10 PM
+# I think it was the ToUpper that was causing problems
+# Don McNamara
+# Jan-24 3:10 PM
+# at least with the signature
+# Don McNamara
+# Jan-24 3:10 PM
+# oh, and the date format thing
+
+
 #Requires -Version 3.0
 
 $script:validUrlCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.~"
 $script:iso8601DateFormat = "yyyy-MM-dd\\THH:mm:ss.fff\\Z"
 
+# http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-query-api.html
 # https://github.com/aws/aws-sdk-net/blob/master/AWSSDK/Amazon.Util/AWSSDKUtils.cs#L779
 function UrlEncode([string]$data, [bool]$path)
 {
